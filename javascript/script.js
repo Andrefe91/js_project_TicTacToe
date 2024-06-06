d97c9743ba5201e14555e233101987c4de253404
Andres Muñoz <andrefe91@gmail.com>
2024-06-06 17:56:24 -0500

    makeMove, chechWinner, getTurnMark and Pretty Print to the script

--- javascript/script.js
+++ javascript/script.js
@@ -8,17 +8,19 @@ function createPlayer (name, number) {
 };
 
 const Board = (function () {
-    let turn = 0;
-    let state = [["","",""],["","",""],["","",""]];
-    const getTurn = () => turn;
+    let turnNumber = 0;
+    let state = [["_","_","_"],["_","_","_"],["_","_","_"]];
+    const getTurnNumber = () => turnNumber;
     const move = (player, coordinates) => {
         if (valid(coordinates)) { //Only apply the move if it's a valid one
             state[coordinates[0]][coordinates[1]] = player.symbol;
-            turn++;
+            turnNumber++;
+        } else {
+            console.log("Invalid move");
         };
     };
     const valid = (coordinates) => (
-        state[coordinates[0]][coordinates[1]] == "" ? true : false
+        (( coordinates[0] < 3 && coordinates[1] < 3) && (state[coordinates[0]][coordinates[1]] == "_") ) ? true : false
     );
     const winCondition = function () { // Check for the win condition
         //Checking rows and columns
@@ -39,7 +41,12 @@ const Board = (function () {
           return false
         };
     };
-    return {state, move, valid, winCondition, getTurn};
+    const prettyPrint = () => {
+        for (let i = 0; i < 3; i++) {
+            console.log(`${state[i][0]} ${state[i][1]} ${state[i][2]}`)
+        };
+    };
+    return {state, move, valid, winCondition, getTurnNumber, prettyPrint};
 })();
 
 // Note: The `turn` property on the returned object is set to the initial value of the `turn` variable at the time the IIFE
@@ -60,16 +67,32 @@ const Game = ( function () {
         player2.name = prompt("Player 2 name");
     };
     const getPlayers = () => [player1, player2];
-    const turn = (coordinates) => {
-        if ((Board.getTurn() % 2) == 0) {
+    const  makeMove= (coordinates) => {
+        if ((Board.getTurnNumber() % 2) == 0) {
             Board.move(player1, coordinates);
         } else {
             Board.move(player2, coordinates)
         };
+        Board.prettyPrint();
     };
-
-
-
-    return {definePlayers, getPlayers, turn};
+    const checkWinner = () => {
+        if (Board.winCondition() == "X") {
+            console.log(`${player1.name} wins!`);
+        } else if (Board.winCondition() == "O") {
+            console.log(`${player2.name} wins!`);
+        } else if (Board.getTurnNumber() == 9) {
+            console.log("It's a draw!");
+        } else {
+            console.log("No winner yet!");
+        };
+    };
+    const getTurnMark = () => {
+        if ((Board.getTurnNumber() % 2) == 0) {
+            return "X";
+        } else {
+            return "O";
+        };
+    };
+    return {definePlayers, getPlayers, makeMove, checkWinner, getTurnMark};
 }) ();
 
